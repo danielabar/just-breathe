2ea3941c3f622c85c9265ba40b80624a491679cd
Daniela Baron <baron.daniela@gmail.com>
2025-07-13 15:13:09 -0400

    adjust rate and pitch to sound more soothing

--- js/voice.js
+++ js/voice.js
@@ -1,8 +1,8 @@
 export function speak(text) {
   if (!('speechSynthesis' in window)) return;
   const utter = new window.SpeechSynthesisUtterance(text);
-  utter.rate = 1;
-  utter.pitch = 1;
+  utter.rate = 0.85;
+  utter.pitch = 0.9;
   utter.lang = 'en-US';
   window.speechSynthesis.cancel(); // Stop any previous utterances
   window.speechSynthesis.speak(utter);
