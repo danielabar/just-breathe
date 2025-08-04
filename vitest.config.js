a4eea7b8329f24db5e144246e080294596a4d66f
Daniela Baron <baron.daniela@gmail.com>
2025-08-04 09:32:05 -0400

    configure test coverage

--- vitest.config.js
+++ vitest.config.js
@@ -3,9 +3,11 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: "jsdom",
+    // https://vitest.dev/config/#coverage
     coverage: {
       provider: "v8",
       reporter: ["html"],
+      include: ["js/**/*.js"],
     },
   },
 });
